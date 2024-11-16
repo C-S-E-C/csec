d769bdc49a3843143840a68ecabd33fc0994a459
ianchzhu <117727274+ianchzhu@users.noreply.github.com>
2024-11-16 11:53:30 +0800

    Delete paypal.js

--- paypal.js
+++
@@ -1,9 +0,0 @@
-document.write('<script src="https://www.paypal.com/sdk/js?client-id=BAAbt14sXdnHKDIDPLigYJzGzKhTxtmouHhG6VJbWt8B6bncMwNEb_4zUPxSV3FYmif7TbheF1kgF4pdmk&components=hosted-buttons&disable-funding=venmo&currency=USD"></script>');
-paypal.HostedButtons({
-  hostedButtonId: "6SBLVDD9P3U56",
-}).render("#paypal-container-6SBLVDD9P3U56")
-/*
-PAYPAL DONATE JS CODE
-ADD THE CODE BELLOW TO HTML
-<div id="paypal-container-6SBLVDD9P3U56"></div>
-*/
