256d9376aac635a4032e19176046433ac2a0c4a7
VFD9654 <117727274+ianchzhu@users.noreply.github.com>
2025-07-27 17:56:14 +0300

    Add files via upload

---
+++ trase.js
@@ -0,0 +1,4 @@
+console.group("CSEC");
+console.log("%c   _____   _____  ______  _____ \n  / ____| / ____||  ____|/ ____|\n | |     | (___  | |__  | |     \n | |      \\___ \\ |  __| | |     \n | |____  ____) || |____| |____ \n  \\_____||_____/ |______|\\_____|\n                                \n                                \n", "color:white;background:black;padding-left:15px;padding-right:15px;");
+console.log("-- csec.top")
+console.groupEnd();
